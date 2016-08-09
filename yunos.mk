24a8af461e797748aec4924ad80b7dba7aa27388
Zhao, Aihua <aihua.zah@alibaba-inc.com>
2016-08-09 16:53:10 +0800

    it is ok to build the static lib only
    Change-Id: I8f37a7469118a601674099eeea5718d3114765af

--- yunos.mk
+++ yunos.mk
@@ -11,26 +11,26 @@ include $(LIBYAMICODEC_PATH)/encoder/yunos.mk
 include $(LIBYAMICODEC_PATH)/vpp/yunos.mk
 # include $(LIBYAMICODEC_PATH)/v4l2/yunos.mk
 
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-include $(LIBYAMICODEC_PATH)/common.mk
-
-LOCAL_SHARED_LIBRARIES := \
-        libva \
-        libva_wayland
-LOCAL_LDFLAGS += -lstdc++
-
-LOCAL_WHOLE_STATIC_LIBRARIES := \
-        libyami_common \
-        libcodecparser \
-        libyami_vaapi \
-        libyami_vpp \
-        libyami_decoder \
-        libyami_encoder
-
-LOCAL_MODULE := libyami
-
-include $(BUILD_SHARED_LIBRARY)
+## LOCAL_PATH := $(call my-dir)
+## include $(CLEAR_VARS)
+## include $(LIBYAMICODEC_PATH)/common.mk
+## 
+## LOCAL_SHARED_LIBRARIES := \
+##         libva \
+##         libva_wayland
+## LOCAL_LDFLAGS += -lstdc++
+## 
+## LOCAL_WHOLE_STATIC_LIBRARIES := \
+##         libyami_common \
+##         libcodecparser \
+##         libyami_vaapi \
+##         libyami_vpp \
+##         libyami_decoder \
+##         libyami_encoder
+## 
+## LOCAL_MODULE := libyami
+## 
+## include $(BUILD_SHARED_LIBRARY)
 
 ## include $(CLEAR_VARS)
 ## include $(LIBYAMICODEC_PATH)/common.mk
