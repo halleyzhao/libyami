2011daae32e14a2c9c10cf684c9f2b5afd9ded50
Zhao, Aihua <aihua.zah@alibaba-inc.com>
2016-08-09 16:58:55 +0800

    update
    Change-Id: Iea9eb4a7e3d6dbe7169867771f874ba590888b93

--- yunos.mk
+++ yunos.mk
@@ -1,3 +1,4 @@
+## static libs for each module
 LOCAL_PATH := $(call my-dir)
 LIBYAMICODEC_PATH := $(LOCAL_PATH)
 include $(CLEAR_VARS)
@@ -11,26 +12,22 @@ include $(LIBYAMICODEC_PATH)/encoder/yunos.mk
 include $(LIBYAMICODEC_PATH)/vpp/yunos.mk
 # include $(LIBYAMICODEC_PATH)/v4l2/yunos.mk
 
-## LOCAL_PATH := $(call my-dir)
-## include $(CLEAR_VARS)
-## include $(LIBYAMICODEC_PATH)/common.mk
-## 
-## LOCAL_SHARED_LIBRARIES := \
-##         libva \
-##         libva_wayland
-## LOCAL_LDFLAGS += -lstdc++
-## 
-## LOCAL_WHOLE_STATIC_LIBRARIES := \
-##         libyami_common \
-##         libcodecparser \
-##         libyami_vaapi \
-##         libyami_vpp \
-##         libyami_decoder \
-##         libyami_encoder
-## 
-## LOCAL_MODULE := libyami
-## 
-## include $(BUILD_SHARED_LIBRARY)
+## libyami
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+include $(LIBYAMICODEC_PATH)/common.mk
+
+LOCAL_SHARED_LIBRARIES := \
+        libva \
+        libva_wayland
+LOCAL_LDFLAGS += -lstdc++
+
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+        libcodecparser
+
+LOCAL_MODULE := libyami
+
+include $(BUILD_SHARED_LIBRARY)
 
 ## include $(CLEAR_VARS)
 ## include $(LIBYAMICODEC_PATH)/common.mk
